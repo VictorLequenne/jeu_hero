3ccde8dacda3353782625288232580eb29c2927c
victor lequenne <victor.lequenne@gmail.com>
2023-10-18 23:35:59 +0200

    initial commit

---
+++ heroTree.js
@@ -0,0 +1,22 @@
+var data = {
+    "00": { choices : ["10: Texte choix 1", "11: Texte choix 2", "12: Texte choix 3","11: etc","11","11","11"],
+            position : [15, 25],
+            end: "No"},
+
+    "10": { choices : ["11: Texte choix 1", "12: Texte choix 2", "13: Texte choix 3"],
+            position : [0,0],
+            end: "No"},
+
+    "11": {choices : ["00: Texte pas le choix"],
+            position : [1,1],
+            end: "Death"},
+
+    "12": {choices : ["00: Texte pas le choix"],
+            position : [1,1],
+            end: "Death"},
+
+    "13": {choices : ["13: WINNER WINNER CHICKEN DINNER"],
+            position : [1,1],
+            end: "Yes"},
+}
+  
---
+++ index.js
@@ -0,0 +1,52 @@
+
+// Const global var : 
+const marginOfError = 0.00015;
+const screenHeight  = window.innerHeight;
+const backgroundColors = ["#D98880", "#D2B4DE", "#AED6F1", "#F9E79F", "#ABEBC6"]
+
+function main(){
+    var player = "00";
+    newEvent(player);
+}
+
+function newEvent(player){
+    console.log(player);
+    const container     = document.getElementById("container");
+    if(data[player].end == "Death") looser();
+    if(data[player].end == "Yes")   winner();
+
+    nchoices = data[player].choices.length;
+    sectionHeight = screenHeight/nchoices;
+    for(i = 0; i < nchoices; i ++){
+        (function (index){
+            const section = document.createElement("div");
+            section.className = "section";
+            section.style.height = sectionHeight + "px";
+            section.style.backgroundColor = backgroundColors[index%backgroundColors.length];
+            section.textContent = data[player].choices[index];
+
+            section.addEventListener("click", function(){
+                console.log(data[player].choices,index,data[player].choices[index]);
+                player = data[player].choices[index].slice(0,2);
+                emptyContainer();
+                newEvent(player);
+            });
+            container.appendChild(section);
+        })(i);
+    }
+}
+
+function looser(){
+    newEvent("00");
+}
+function winner(){
+    emptyContainer();
+}
+function emptyContainer() {
+    const container = document.getElementById("container");
+  
+    // Remove all child elements from the container
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+}
