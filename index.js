4ff205dc62c6ce4ed4453ae4661de8f573d02534
victor lequenne <victor.lequenne@gmail.com>
2023-10-20 21:31:00 +0200

    ''

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // Const global var : 
-const meterMargin = 20;
+const meterMargin = 5;
 const marginOfError = (meterMargin / 40075000) * 360;
 console.log("Margin of error is:", marginOfError);
 const screenHeight  = window.innerHeight;
