6798a515f4864b600ad1624a699eb7f3fc69e57d
victor lequenne <victor.lequenne@gmail.com>
2023-10-20 19:55:47 +0200

    test tel taille texte

--- heroTree.js
+++ heroTree.js
@@ -1,22 +1,27 @@
 var data = {
-    "00": { choices : ["10: Texte choix 1", "11: Texte choix 2", "12: Texte choix 3","11: etc","11","11","11"],
-            position : [15, 25],
+    "00": { choices : ["10: Vous allez vers le cimetiere", "11: Vous vous dirigez vers l'hôpital"],
+            text : "Bienvenue a Mickey Town, la premiere personne que vous recontrez est le policier, il vous dit qu'en cas de soucis vous pouvez toujours venir vers lui, par exemple si vous avez qq un a mettre en sécurité, vous le remercier pour sa gentillesse et sa bienveillance et ",
+            position : [50.5351671, 4.9159056, "la place"],
             end: "No"},
 
-    "10": { choices : ["11: Texte choix 1", "12: Texte choix 2", "13: Texte choix 3"],
-            position : [0,0],
+    "10": { choices : ["20: Vous décidez d'aller avec eux", "21: Vous partez de votre coté"],
+            text : "Vous entrez dans l'hôpital à l'entrée de mickey town et decidez de vous y reposer un peu, apres qq minutes, 3 freres: Lulu pipi et coco font leur apparition. Vous discutez un peu avec eux et au fil de la discussion, vous apprenez qu'ils ont rendez-vous avec minnie, ils vous proposent d'y aller avec eux",
+            position : [50.5351671, 4.9159056],
             end: "No"},
 
     "11": {choices : ["00: Death"],
-            position : [1,1],
+            text : "blablabla",
+            position : [50.5351671, 4.9159056],
             end: "Death"},
 
     "12": {choices : ["00: Death"],
-            position : [1,1],
+            text : "blablabla",
+            position : [50.5351671, 4.9159056],
             end: "Death"},
 
     "13": {choices : ["13: WINNER WINNER CHICKEN DINNER"],
-            position : [1,1],
+            text : "blablabla",
+            position : [50.5351671, 4.9159056],
             end: "Yes"},
 }
   
--- index.js
+++ index.js
@@ -1,47 +1,143 @@
 
 // Const global var : 
-const marginOfError = 0.00015;
+const meterMargin = 2000000000000;
+const marginOfError = (meterMargin / 40075000) * 360;
+console.log("Margin of error is:", marginOfError);
 const screenHeight  = window.innerHeight;
+const screenWidth   = window.innerWidth;
 const backgroundColors = ["#D98880", "#D2B4DE", "#AED6F1", "#F9E79F", "#ABEBC6"]
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000000,
+    maximumAge: 1000,
+};
+  
 
+// Main function that is called when entering the page
 function main(){
     var player = "00";
     newEvent(player);
 }
 
-function newEvent(player){
+
+function newEvent(player) {
     console.log(player);
-    const container     = document.getElementById("container");
-    if(data[player].end == "Death") looser();
-    if(data[player].end == "Yes")   winner();
-
-    nchoices = data[player].choices.length;
-    sectionHeight = screenHeight/nchoices;
-    for(i = 0; i < nchoices; i ++){
-        (function (index){
-            const section = document.createElement("div");
-            section.className = "section";
-            section.style.height = sectionHeight + "px";
-            section.style.backgroundColor = backgroundColors[index%backgroundColors.length];
-            section.textContent = data[player].choices[index];
-
-            section.addEventListener("click", function(){
-                console.log(data[player].choices,index,data[player].choices[index]);
-                player = data[player].choices[index].slice(0,2);
+    const container = document.getElementById("container");
+    if (data[player].end === "Death") displayDeathScreen(container,player);
+    else if (data[player].end === "Yes") displayWinScreen(container,player);
+    else{
+        // Call isInPosition with a callback function to handle displaying choices
+        isInPosition(player, function (isClose) {
+            if (isClose) {
+                const nchoices = data[player].choices.length;
+                const sectionHeight = screenHeight / (nchoices + 1);
                 emptyContainer();
+                const textSection = add_text(player,sectionHeight);
+                container.appendChild(textSection);
+                for (let i = 0; i < nchoices; i++) {
+                    const newSection = add_choice(player, i, sectionHeight);
+                    container.appendChild(newSection);
+                }
+            }else{
+                const newSection = displayGoToScreen(player);
+                container.appendChild(newSection);
                 newEvent(player);
-            });
-            container.appendChild(section);
-        })(i);
+            }
+        });
     }
 }
 
-function looser(){
-    emptyContainer();
+function displayGoToScreen(player){
+    const section = document.createElement("div");
+    section.className = "section";
+    section.style.height = screenHeight + "px";
+    section.style.backgroundColor = backgroundColors[0];
+    section.textContent = "Allez a " + data[player].position[2];
+    return section;
 }
-function winner(){
+
+function displayDeathScreen(container,player){
     emptyContainer();
+    const section = document.createElement("div");
+    section.className = "section";
+    section.style.height = screenHeight + "px";
+    section.style.backgroundColor = backgroundColors[0];
+    section.textContent = "Vous êtes mort, triste";
+    container.appendChild(section);
+    section.addEventListener("click", function(){
+        player = data[player].choices[0].slice(0,2);
+        emptyContainer();
+        newEvent(player);
+    });
+    
 }
+function displayWinScreen(container,player){
+    const section = document.createElement("div");
+    section.className = "section";
+    section.style.height = screenHeight + "px";
+    section.style.backgroundColor = backgroundColors[0];
+    section.textContent = "Wouhouhou vous avez démoli mickey";
+    container.appendChild(section);
+}
+
+function isInPosition(player, callback) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const current_position = position.coords;
+            const aim_position = data[player].position;
+            const dist_squared = dist2(
+                current_position.latitude,
+                current_position.longitude,
+                aim_position[0],
+                aim_position[1]
+            );
+            console.log("debug");
+            console.log(current_position);
+            console.log(aim_position);
+            console.log(dist_squared);
+            
+            // Call the callback with a boolean value indicating if the player is close
+            callback(dist_squared <= marginOfError);
+        },
+        error,
+        options
+    );
+}
+
+
+// Return the squared distance of 2 points
+function dist2(x0,y0,x1,y1) {return (x0 - x1)**2 + (y0 - y1)**2;}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+  console.log("AIEEE");
+}
+
+function add_text(player, sectionHeight){
+    const section = document.createElement("div");
+    section.className = "section";
+    section.style.height = sectionHeight + "px";
+    section.style.backgroundColor = backgroundColors[0];
+    section.textContent = data[player].text;
+    return section
+}
+function add_choice(player,index,sectionHeight,sectionWidth){
+    const section = document.createElement("div");
+    section.className = "section";
+    section.style.height = sectionHeight + "px";
+    section.style.backgroundColor = backgroundColors[(1+index)%backgroundColors.length];
+    section.textContent = data[player].choices[index];
+
+    section.addEventListener("click", function(){
+        console.log(data[player].choices,index,data[player].choices[index]);
+        player = data[player].choices[index].slice(0,2);
+        emptyContainer();
+        newEvent(player);
+    });
+    return section
+}
+
+
 function emptyContainer() {
     const container = document.getElementById("container");
   
